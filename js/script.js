16f40492b657d5436fab4eae0be4bb8700042e70
Bossphourus <alperen.oval@gmail.com>
2024-05-01 01:19:46 +0300

    update script.js

--- js/script.js
+++ js/script.js
@@ -60,7 +60,7 @@ function getExchangeRate() {
     amountVal = 1;
   }
   exchangeRateTxt.innerText = "Getting exchange rate...";
-  let url = `https://v6.exchangerate-api.com/v6/YOUR-API-KEY/latest/${fromCurrency.value}`;
+  let url = `https://v6.exchangerate-api.com/v6/a7db3a1697d3fb42d07b6a884a802888/latest/${fromCurrency.value}`;
   fetch(url)
     .then((response) => response.json())
     .then((result) => {
