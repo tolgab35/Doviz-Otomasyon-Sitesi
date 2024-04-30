3e07197998fc806267308edbcca7185cb8297dd5
Bossphourus <alperen.oval@gmail.com>
2024-05-01 01:26:35 +0300

    update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,2 +1,13 @@
 <?php
-    ?>
+
+    $dsn = "mysql:host=localhost;dbname=doviz_otomasyonu";
+    $dbusername = "root1";
+    $dbpassword = "123456"; 
+
+    try {
+        $pdo = new PDO($dsn, $dbusername, $dbpassword);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) { 
+        echo "Bağlantı başarısız". $e->getMessage();
+    }
+?>
