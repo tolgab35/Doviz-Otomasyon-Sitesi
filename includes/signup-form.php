300076edfcce1ec28e679c85dd7a835d1cca3971
Tolga <tolgaboz350@gmail.com>
2024-05-01 04:42:58 +0300

    add forms in hesap.html

--- includes/signup-form.php
+++ includes/signup-form.php
@@ -10,10 +10,17 @@
             require_once "dbh.inc.php";
 
             $query = "INSERT INTO kullanici (kullanici_adi, kullanici_soyadi, kullanici_email, kullanici_tel, kullanici_sifre) 
-            VALUES (?, ?, ?, ?, ?);";
+            VALUES (:kullanici_adi, :kullanici_soyadi, :kullanici_email, :kullanici_tel, :kullanici_sifre);";
 
             $stmt = $pdo->prepare($query);
-            $stmt->execute([$firstname, $lastname, $email, $phone, $password]);
+
+            $stmt->bindParam(":kullanici_adi", $firstname);
+            $stmt->bindParam(":kullanici_soyadi", $lastname);
+            $stmt->bindParam(":kullanici_email", $email);
+            $stmt->bindParam(":kullanici_tel", $phone);
+            $stmt->bindParam(":kullanici_sifre", $password);
+
+            $stmt->execute();
 
             $pdo = null;
             $stmt = null;
