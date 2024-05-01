0ee5f9fd83620dc568070fc9ba31a41359dd8f00
Tolga <tolgaboz350@gmail.com>
2024-05-01 05:33:49 +0300

    update hesap.html

--- includes/signup-form.php
+++ includes/signup-form.php
@@ -10,17 +10,10 @@
             require_once "dbh.inc.php";
 
             $query = "INSERT INTO kullanici (kullanici_adi, kullanici_soyadi, kullanici_email, kullanici_tel, kullanici_sifre) 
-            VALUES (:kullanici_adi, :kullanici_soyadi, :kullanici_email, :kullanici_tel, :kullanici_sifre);";
+            VALUES (?, ?, ?, ?, ?);";
 
             $stmt = $pdo->prepare($query);
-
-            $stmt->bindParam(":kullanici_adi", $firstname);
-            $stmt->bindParam(":kullanici_soyadi", $lastname);
-            $stmt->bindParam(":kullanici_email", $email);
-            $stmt->bindParam(":kullanici_tel", $phone);
-            $stmt->bindParam(":kullanici_sifre", $password);
-
-            $stmt->execute();
+            $stmt->execute([$firstname, $lastname, $email, $phone, $password]);
 
             $pdo = null;
             $stmt = null;
